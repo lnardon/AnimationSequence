6efe279c5b19d20db2b19f13a5c5e2223b5e4bf9
lnardon <lucaslnardon@gmail.com>
2021-03-03 03:59:25 -0300

    ✨ Finish basic package functionality

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 export default function setSequence(
   classNameToTrack: string,
-  classNameWithAnimation: string
+  keyframesName: string,
+  delay: number
 ) {
-  const elems = document.querySelectorAll("." + classNameToTrack);
-  elems.forEach((el) => {
-    el.classList.add(classNameWithAnimation);
+  const elems = Array.from(
+    document.getElementsByClassName(
+      classNameToTrack
+    ) as HTMLCollectionOf<HTMLElement>
+  );
+  elems.forEach((el, index) => {
+    el.style.animationName = keyframesName;
+    el.style.animationDuration = "1s";
+    el.style.animationDelay = index * delay + "s";
   });
 }
